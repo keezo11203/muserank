792beb2028be53f01964a79874dcae0b6c58b166
keezo11203 <kaitlynellis0813@gmail.com>
2024-07-02 12:43:05 -0400

    Initial Commit

---
+++ MuseRank/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MuseRank
+//
+//  Created by Kaitlyn Ellis on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MuseRank/MuseRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseRankApp.swift
+//  MuseRank
+//
+//  Created by Kaitlyn Ellis on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MuseRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
