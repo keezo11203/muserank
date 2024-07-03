b7cbdf19517dc3cb08d07e3ce7e931f3308de3a1
keezo11203 <kaitlynellis0813@gmail.com>
2024-07-03 15:19:28 -0400

    album titles

--- MuseRank/AlbumViewModel.swift
+++ MuseRank/AlbumViewModel.swift
@@ -10,16 +10,16 @@ import SwiftUI
 
 class AlbumViewModel: ObservableObject {
     @Published var albums: [Album] = [
-        Album(title: "Album 1", artist: "Artist 1", coverImage: "cover1", rating: nil, review: ""),
-        Album(title: "Album 2", artist: "Artist 2", coverImage: "cover2", rating: nil, review: ""),
-        Album(title: "Album 3", artist: "Artist 3", coverImage: "cover3", rating: nil, review: ""),
-        Album(title: "Album 4", artist: "Artist 4", coverImage: "cover4", rating: nil, review: ""),
-        Album(title: "Album 5", artist: "Artist 5", coverImage: "cover5", rating: nil, review: ""),
-        Album(title: "Album 6", artist: "Artist 6", coverImage: "cover6", rating: nil, review: ""),
-        Album(title: "Album 7", artist: "Artist 7", coverImage: "cover7", rating: nil, review: ""),
-        Album(title: "Album 8", artist: "Artist 8", coverImage: "cover8", rating: nil, review: ""),
-        Album(title: "Album 9", artist: "Artist 9", coverImage: "cover9", rating: nil, review: ""),
-        Album(title: "Album 10", artist: "Artist 10", coverImage: "cover10", rating: nil, review: ""),
+        Album(title: "HIT ME HARD AND SOFT", artist: "Artist 1", coverImage: "cover1", rating: nil, review: ""),
+        Album(title: "eternal sunshine", artist: "Artist 2", coverImage: "cover2", rating: nil, review: ""),
+        Album(title: "COWBOY CARTER", artist: "Artist 3", coverImage: "cover3", rating: nil, review: ""),
+        Album(title: "brat", artist: "Artist 4", coverImage: "cover4", rating: nil, review: ""),
+        Album(title: "TYLA", artist: "Artist 5", coverImage: "cover5", rating: nil, review: ""),
+        Album(title: "MEGAN", artist: "Artist 6", coverImage: "cover6", rating: nil, review: ""),
+        Album(title: "BB/ANG3L", artist: "Artist 7", coverImage: "cover7", rating: nil, review: ""),
+        Album(title: "Radical Optimism", artist: "Artist 8", coverImage: "cover8", rating: nil, review: ""),
+        Album(title: "The Rise and Fall of a Midwest Princess", artist: "Artist 9", coverImage: "cover9", rating: nil, review: ""),
+        Album(title: "THINK LATER", artist: "Artist 10", coverImage: "cover10", rating: nil, review: ""),
 
 
     ]
